1caf4eec00f15989ba110588b8afead2245e368c
jakub.pawlowski <jakub.pawlowski@legimi.com>
2015-05-20 15:40:44 +0200

    hack: make ConfigureDataProtectorExtension public

--- source/Core/Configuration/AppBuilderExtensions/ConfigureDataProtectorExtension.cs
+++ source/Core/Configuration/AppBuilderExtensions/ConfigureDataProtectorExtension.cs
@@ -21,7 +21,7 @@ using Thinktecture.IdentityServer.Core.Configuration.Hosting;
 
 namespace Owin
 {
-    internal static class ConfigureDataProtectorExtension
+    public static class ConfigureDataProtectorExtension
     {
         public static IAppBuilder ConfigureDataProtectionProvider(this IAppBuilder app, IdentityServerOptions options)
         {
